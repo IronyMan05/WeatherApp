4ad4b98f871c43080aecaf152e391d95fec385aa
Muhammad Yusuf <90136884+IronyMan05@users.noreply.github.com>
2024-04-25 23:35:50 +0530

    Add files via upload

---
+++ utils/weatherData.js
@@ -0,0 +1,24 @@
+
+const request = require("request");
+
+const openWeatherMap = {
+  BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+  SECRET_KEY: "30e18b33dd15bbb9a55f76d1a7a1111b",
+};
+
+const weatherData = (address, callback) => {
+  const url =
+    openWeatherMap.BASE_URL +
+    encodeURIComponent(address) +
+    "&APPID=" +
+    openWeatherMap.SECRET_KEY;
+  console.log(url);
+  request({ url, json: true }, (error, data) => {
+    if (error) {
+      callback(true, "Unable to fetch data, Please try again. " + error);
+    }
+    callback(false, data?.body);
+  });
+};
+
+module.exports = weatherData;
